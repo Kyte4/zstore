2516aced4138913a7673719ae9bd2abff75f2dc0
Georgii <kek_888@mail.ru>
2024-12-26 02:40:10 +0300

    WOW CHTO ETO

--- server.js
+++ server.js
@@ -1,59 +1,29 @@
-// const express = require('express');
-// const sql = require('mysql');
-// const app = express();
-
-// const dbConfig = {
-//   user: 'kyte4',
-//   password: '',
-//   server: 'localhost',
-//   database: 'zstore',
-//   options: {
-//     encrypt: true, // Установите в true для Azure SQL
-//     enableArithAbort: true
-//   }
-// };
-
-// async function getProducts(req, res) {
-//   try {
-//     const pool = await sql.connect(dbConfig);
-//     const result = await pool.request().query('SELECT id, name, price, quantity FROM products');
-//     res.json(result.recordset);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// }
-
-// // Настройте маршруты после объявления функции getProducts
-// app.get('/api/products', getProducts);
-
-// app.use(express.static('public'));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Сервер запущен на порту ${PORT}`);
-// });
 const express = require('express');
-const mysql = require('mysql2');
+const sql = require('mssql');
 const app = express();
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: '127.0.0.1',
+const dbConfig = {
   user: 'kyte4',
   password: '',
+  server: '127.0.0.1', // Или имя вашего сервера
   database: 'zstore',
-  connectTimeout: 10000,
-  charset: 'utf8mb4'
-});
+  options: {
+    encrypt: true, // Установите в true для Azure SQL
+    enableArithAbort: true
+  }
+};
 
-app.get('/api/products', (req, res) => {
-  pool.query('SELECT id, name, price, quantity FROM products', (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err });
-    }
-    res.json(results);
-  });
-});
+async function getProducts(req, res) {
+  try {
+    await sql.connect(dbConfig);
+    const result = await sql.query('SELECT id, name, price, quantity FROM products');
+    res.json(result.recordset);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+app.get('/api/products', getProducts);
 
 app.use(express.static('public'));
 
@@ -61,4 +31,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
-
