5c84ab47b3953a21406eacf7e7a8cd21fc63587c
Georgii Goncharov <97386643+Kyte4@users.noreply.github.com>
2025-06-24 12:02:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -307,7 +307,7 @@ const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'ZStore API',
+      title: 'Store API',
       version: '1.0.0',
       description: 'Документация API для ZStore',
     },
