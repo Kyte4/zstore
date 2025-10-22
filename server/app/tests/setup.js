a970720bd36b9ab02c93597b71f2969171f2e906
Georgii <goncharovgeorgii03@gmail.com>
2025-10-22 18:13:14 +0300

    Enhance test setup to use JWT configuration and token generation

--- server/app/tests/setup.js
+++ server/app/tests/setup.js
@@ -1,8 +1,10 @@
 import sequelize from '../config/dbConfig.js';
 import { jwt, JWT_SECRET, JWT_EXPIRES_IN } from '../config/JWT.js';
+
 afterAll(async () => {
   await sequelize.close();
 });
+
 beforeAll(() => {
   token = jwt.sign({ id: 1, username: 'testuser' }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 });
