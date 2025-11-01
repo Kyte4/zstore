a3cc97f4a435809311e30900e481873ea13aa2e9
Georgii <goncharovgeorgii03@gmail.com>
2025-11-02 00:38:04 +0300

    Test

--- server/app/jest.config.js
+++ server/app/jest.config.js
@@ -5,4 +5,11 @@ export default {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   moduleFileExtensions: ['js', 'mjs', 'json', 'node'],
+  testMatch: [
+    '**/tests/**/*.test.mjs',
+    '**/tests/**/*.test.js',
+    '**/__tests__/**/*.mjs',
+    '**/__tests__/**/*.js',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/build/', '/dist/'],
 };
