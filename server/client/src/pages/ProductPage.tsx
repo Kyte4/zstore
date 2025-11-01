0652ff46348946c781aceabae8d7cdfde448dcf7
Georgii <goncharovgeorgii03@gmail.com>
2025-11-02 01:11:46 +0300

    Eslint

--- server/client/src/pages/ProductPage.tsx
+++ server/client/src/pages/ProductPage.tsx
@@ -1,6 +1,6 @@
 // src/pages/ProductPage.tsx
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import '../styles/product.css';
 import type { Product, Notification } from '../types/types';
 
